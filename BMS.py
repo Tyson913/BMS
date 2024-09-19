11e123843e48f3546785b0e6b0f935d0ab286790
Cj Banluta <135036754+Tyson913@users.noreply.github.com>
2024-09-19 22:04:46 +0800

    added a logic

--- BMS.py
+++ BMS.py
@@ -3,38 +3,101 @@
 # SP = Sign Up
 # LN = Log In
 
-class BMS:
+import json
+
+class BMS: # Bank Management System Object
     # Initiallize the BMS
     def __init__(self, accounts, balance):
         self.accounts = accounts
         self.balance = balance
     
+    # Made a function for getting the chosen user auth of the user (Log In and Sign Up)
+    def getUserAuth(self):
+        print("1. Sign Up\n2. Log In")
+        chosenUserAuth = int(input("Enter a number: "))
+
+        return chosenUserAuth
+
     # Made a function for getting the signed up username of the user
     def getSPUsername(self):
         SPUsername = input("Enter your desired username: ")
         return SPUsername
     
+    # Made a function for getting the created password of the user
     def getSPPassword(self):
         SPPassword = input("Enter a password: ")
         SPPasswordConfirmation = input("Confirm your password: ")
 
-    def SPUsernameisValid(self, signedUpUsername):
-        if len(signedUpUsername) >= 6:
+        return SPPassword, SPPasswordConfirmation
+
+    # Function for the validation of Signed Up Username of the user
+    def SPUsernameisValid(self, SPusername):
+        if len(SPusername) >= 6:
             return True
         return False
+    
+    # Function for the validation of Signed Up password of the user
+    def SPPasswordisValid(self, SPPassword, SPPasswordConfirmation):
+        if len(SPPassword) and len(SPPasswordConfirmation) >= 6 and SPPasswordConfirmation == SPPassword:
+            return True
+        return False
+    
+    # Function to print out the offered services
+    def Services(self):
+        Services = '''
+            ======================================
+            ||             Services             ||
+            ======================================
+            ||            1. Deposit            ||
+            ||            2. Transfer           ||
+            ||            3. Withdraw           ||
+            ||      4. Transaction History      ||
+            ======================================
+
+        '''
+        print(Services)
+        return None
+    
+    def addAcc(self, SPUsername, SPPassword):
+        self.accounts = []
+        account = {
+            "username": SPUsername,
+            "password": SPPassword
+        }
+
+        self.accounts.append(account)
+
+        with open('BMS.json', 'w') as file:
+            json.dump(self.accounts,file)
+
 
 
 def main():
+    print(" ")
     # Created a temporary instance for the BMS
     bms = BMS(accounts=True, balance=True)
 
-    # Declared the getSPUsername function inside the class
-    signedUpUsername = bms.getSPUsername()
+    # Collected the username of the user inside the getSPUsername function inside the class
+    while bms.getUserAuth() != 1 and bms.getUserAuth() != 2:
+        print("Please enter a valid number.")
+        bms.getUserAuth()
+    while True:
+        if bms.getUserAuth() == 1:
+            SPUsername = bms.getSPUsername() # if the chosen user auth is 1 then perform the username validation
+            if bms.SPUsernameisValid(SPUsername): # if the SPUsername is valid, the getSPPassword will be executed
+                SPPassword, SPPasswordConfirmation = bms.getSPPassword() # Get the values of the variables SPPassword and SPPasswordConfirmation
+                if bms.SPPasswordisValid(SPPassword, SPPasswordConfirmation): # if the password created by the user is valid, then the function Services will be executed
+                    bms.addAcc(SPUsername, SPPassword)
+                    bms.Services()
+                    break
+                
+            break
+        elif bms.getUserAuth() == 2:
 
-    if bms.SPUsernameisValid(signedUpUsername):
-        print("The password is valid")
-    else:
-        print("Invalid Password. Password should have atleast 6 characters")
+
+            # Code for the log In user auth here
+            break # Temporary code to avoid error
+
 
 if __name__ == "__main__":
     main()
